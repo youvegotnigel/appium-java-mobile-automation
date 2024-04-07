b43e0f367226846e25b1a82c9ad8a943a3e63e8c
Nigel <nigeldave19@gmail.com>
2024-04-08 00:44:11 +0530

    Update WDIO_IOS.java

--- src/test/java/org/nigel/automation/WDIO_IOS.java
+++ src/test/java/org/nigel/automation/WDIO_IOS.java
@@ -1,17 +1,31 @@
 package org.nigel.automation;
 
+import org.nigel.automation.pageobjects.WDIOHomePage;
+import org.nigel.automation.pageobjects.WDIOLoginPage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 
 public class WDIO_IOS extends TestBase{
 
+    WDIOHomePage wdioHomePage;
+    WDIOLoginPage wdioLoginPage;
 
     @Test
-    public void add_item_to_cart() throws MalformedURLException {
+    public void test_login_function() throws MalformedURLException {
 
         iOS_setUp();
+        wdioHomePage = new WDIOHomePage(driver);
+        wdioLoginPage = new WDIOLoginPage(driver);
 
+        wdioHomePage.clickLoginIcon();
+        wdioLoginPage.enterEmail("test@wdio.com");
+        wdioLoginPage.enterPassword("superStrongPa$$w0rd");
+        wdioLoginPage.clickLoginBtn();
+        Assert.assertTrue(wdioLoginPage.isSuccessMessagePopupDispalyed());
+        wdioLoginPage.clickOkBtn();
+        Assert.assertFalse(wdioLoginPage.isSuccessMessagePopupDispalyed());
         tearDown();
     }
 
