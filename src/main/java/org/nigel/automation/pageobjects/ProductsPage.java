4e20b84b2e64251463463b1950192ea40f231fc2
Nigel <nigeldave19@gmail.com>
2024-04-07 21:40:12 +0530

    Create ProductsPage.java

---
+++ src/main/java/org/nigel/automation/pageobjects/ProductsPage.java
@@ -0,0 +1,13 @@
+package org.nigel.automation.pageobjects;
+
+import io.appium.java_client.AppiumDriver;
+
+public class ProductsPage extends PageBase {
+
+    public ProductsPage(AppiumDriver appiumDriver) {
+        super(appiumDriver);
+    }
+
+
+
+}
