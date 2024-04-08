f0daea7164992f7cec65b254783311a2abf04a2a
Nigel <nigeldave19@gmail.com>
2024-04-09 00:35:16 +0530

    Update IosDriverService.java

--- src/main/java/org/nigel/automation/driver/IosDriverService.java
+++ src/main/java/org/nigel/automation/driver/IosDriverService.java
@@ -3,6 +3,7 @@ package org.nigel.automation.driver;
 import io.appium.java_client.ios.IOSDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
@@ -20,9 +21,12 @@ public class IosDriverService {
         capabilities.setCapability("platformVersion", "15.0");
         capabilities.setCapability("udid", "0A91734C-D156-448B-BBC2-739003C9B4ED");
         capabilities.setCapability("bundleID", "org.wdioNativeDemoApp");
-        //capabilities.setCapability("app",
-        //System.getProperty("user.dir") + File.separator + "apps" + File.separator + "SauceLabs-Demo-App.ipa");
-        //capabilities.setCapability(NO_REST, true);
+        capabilities.setCapability("app",
+                System.getProperty("user.dir") + File.separator + "apps" + File.separator + "wdioNativeDemoApp.app");
+
+        capabilities.setCapability("noReset", "false");
+        capabilities.setCapability("fullReset", "false");
+        capabilities.setCapability("autoAcceptAlerts", "true");
 
 
         try {
