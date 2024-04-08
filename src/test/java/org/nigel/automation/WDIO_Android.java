264d804451e14a7ef9f8649dc6cdca6954f5c0e8
Nigel <nigeldave19@gmail.com>
2024-04-08 11:29:11 +0530

    Create WDIO_Android.java

---
+++ src/test/java/org/nigel/automation/WDIO_Android.java
@@ -0,0 +1,35 @@
+package org.nigel.automation;
+
+import org.nigel.automation.pageobjects.WDIOHomePage;
+import org.nigel.automation.pageobjects.WDIOLoginPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+
+public class WDIO_Android extends TestBase{
+
+    WDIOHomePage wdioHomePage;
+    WDIOLoginPage wdioLoginPage;
+
+    @Test
+    public void test_login_function() throws MalformedURLException, InterruptedException {
+
+        Android_wdio_setUp();
+        wdioHomePage = new WDIOHomePage(driver);
+        wdioLoginPage = new WDIOLoginPage(driver);
+
+        wdioHomePage.clickLoginIcon();
+        wdioLoginPage.enterEmail("test@wdio.com");
+        wdioLoginPage.enterPassword("superStrongPa$$w0rd");
+        wdioLoginPage.clickLoginBtn();
+        Thread.sleep(1000);
+        Assert.assertTrue(wdioLoginPage.isSuccessMessagePopupDisplayed());
+        wdioLoginPage.clickOkBtn();
+        Thread.sleep(1000);
+        Assert.assertFalse(wdioLoginPage.isSuccessMessagePopupDisplayed());
+        tearDown();
+    }
+
+
+}
