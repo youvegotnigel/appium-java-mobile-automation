2bbfefe6e27e1bd0f44b876fa73144c5a7d138d2
Nigel <nigeldave19@gmail.com>
2024-04-10 23:03:22 +0530

    Create GestureTest.java

---
+++ src/test/java/org/nigel/automation/gestures/GestureTest.java
@@ -0,0 +1,81 @@
+package org.nigel.automation.gestures;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.options.XCUITestOptions;
+import io.appium.java_client.remote.AutomationName;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Pause;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.Collections;
+
+public class GestureTest {
+
+
+    @Test
+    public void test_for_tab() throws MalformedURLException {
+
+        XCUITestOptions options = new XCUITestOptions();
+        options.setPlatformName("iOS"); //optional
+        options.setAutomationName(AutomationName.IOS_XCUI_TEST); //optional
+        options.setDeviceName("iPhone 13");
+        options.setPlatformVersion("15.0");
+        options.setApp(System.getProperty("user.dir") + File.separator + "apps" + File.separator + "SauceLabs-Demo-App.Simulator.zip");
+
+        IOSDriver driver = new IOSDriver(new URL("http://127.0.0.1:4723"), options);
+
+        driver.findElement(AppiumBy.accessibilityId("Cart-tab-item")).click();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        WebElement goShoppingButton = driver.findElement(AppiumBy.accessibilityId("Go Shopping"));
+        Assert.assertTrue(goShoppingButton.isDisplayed());
+
+        // tap on go shopping button
+        tap(driver, goShoppingButton);
+
+        String productText = driver.findElement(AppiumBy.accessibilityId("Products")).getText();
+        Assert.assertEquals(productText, "Products");
+        driver.quit();
+    }
+
+
+
+    private void tap(AppiumDriver driver, WebElement element) {
+
+        Point location = element.getLocation();
+        Dimension size = element.getSize();
+
+        Point centerOfElement = getCenterOfElement(location, size);
+
+        PointerInput finger1 = new PointerInput(PointerInput.Kind.TOUCH, "finger1");
+        Sequence sequence = new Sequence(finger1, 1)
+                .addAction(finger1.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), centerOfElement))
+                .addAction(finger1.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
+                .addAction(new Pause(finger1, Duration.ofMillis(200)))
+                .addAction(finger1.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+        driver.perform(Collections.singletonList(sequence));
+
+    }
+
+    private Point getCenterOfElement(Point location, Dimension size) {
+        return new Point(location.getX() + size.getWidth() / 2,
+                location.getY() + size.getHeight() / 2);
+    }
+
+}
