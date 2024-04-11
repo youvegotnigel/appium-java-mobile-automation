6e17ada764a7c6bdac83670ec8eaaee016d2a29f
Nigel <nigeldave19@gmail.com>
2024-04-11 13:48:45 +0530

    added test for double tap

--- src/test/java/org/nigel/automation/gestures/GestureTest.java
+++ src/test/java/org/nigel/automation/gestures/GestureTest.java
@@ -22,6 +22,8 @@ import java.util.Collections;
 
 public class GestureTest {
 
+    private static final String IOS_SAUCE_LABS_DEMO_APP_PATH = System.getProperty("user.dir") + File.separator + "apps" + File.separator + "SauceLabs-Demo-App.Simulator.zip";
+    private static final String APPIUM_URL = "http://127.0.0.1:4723";
 
     @Test
     public void test_for_tab() throws MalformedURLException {
@@ -31,9 +33,9 @@ public class GestureTest {
         options.setAutomationName(AutomationName.IOS_XCUI_TEST); //optional
         options.setDeviceName("iPhone 13");
         options.setPlatformVersion("15.0");
-        options.setApp(System.getProperty("user.dir") + File.separator + "apps" + File.separator + "SauceLabs-Demo-App.Simulator.zip");
+        options.setApp(IOS_SAUCE_LABS_DEMO_APP_PATH);
 
-        IOSDriver driver = new IOSDriver(new URL("http://127.0.0.1:4723"), options);
+        IOSDriver driver = new IOSDriver(new URL(APPIUM_URL), options);
 
         driver.findElement(AppiumBy.accessibilityId("Cart-tab-item")).click();
 
@@ -53,6 +55,29 @@ public class GestureTest {
         driver.quit();
     }
 
+    @Test
+    public void test_for_doubleTab() throws MalformedURLException {
+
+        XCUITestOptions options = new XCUITestOptions();
+        options.setDeviceName("iPhone 13");
+        options.setPlatformVersion("15.0");
+        options.setApp(IOS_SAUCE_LABS_DEMO_APP_PATH);
+
+        IOSDriver driver = new IOSDriver(new URL(APPIUM_URL), options);
+
+        driver.findElement(AppiumBy.accessibilityId("Sauce Labs Bike Light")).click();
+
+        WebElement plusIcon = driver.findElement(AppiumBy.accessibilityId("AddPlus Icons"));
+        Assert.assertTrue(plusIcon.isDisplayed());
+
+        // tap on go shopping button
+        doubleTap(driver, plusIcon);
+
+        String amountText = driver.findElement(AppiumBy.accessibilityId("Amount")).getText();
+        Assert.assertEquals(amountText, "3");
+        driver.quit();
+    }
+
 
 
     private void tap(AppiumDriver driver, WebElement element) {
@@ -64,13 +89,33 @@ public class GestureTest {
 
         PointerInput finger1 = new PointerInput(PointerInput.Kind.TOUCH, "finger1");
         Sequence sequence = new Sequence(finger1, 1)
-                .addAction(finger1.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), centerOfElement))
-                .addAction(finger1.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
-                .addAction(new Pause(finger1, Duration.ofMillis(200)))
-                .addAction(finger1.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+                .addAction(finger1.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), centerOfElement)) // move finger to button from viewport
+                .addAction(finger1.createPointerDown(PointerInput.MouseButton.LEFT.asArg())) // move finger down and click (similar to mouse left click)
+                .addAction(new Pause(finger1, Duration.ofMillis(200))) // click and hold for a small duration
+                .addAction(finger1.createPointerUp(PointerInput.MouseButton.LEFT.asArg())); // move finger away from button
 
         driver.perform(Collections.singletonList(sequence));
+    }
 
+    private void doubleTap(AppiumDriver driver, WebElement element) {
+
+        Point location = element.getLocation();
+        Dimension size = element.getSize();
+
+        Point centerOfElement = getCenterOfElement(location, size);
+
+        PointerInput finger1 = new PointerInput(PointerInput.Kind.TOUCH, "finger1");
+        Sequence sequence = new Sequence(finger1, 1)
+                .addAction(finger1.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), centerOfElement)) // move finger to button from viewport
+                .addAction(finger1.createPointerDown(PointerInput.MouseButton.LEFT.asArg())) // move finger down and click (similar to mouse left click)
+                .addAction(new Pause(finger1, Duration.ofMillis(100))) // click and hold for a small duration
+                .addAction(finger1.createPointerUp(PointerInput.MouseButton.LEFT.asArg())) // move finger away from button
+                .addAction(new Pause(finger1, Duration.ofMillis(100))) // click and hold for a small duration
+                .addAction(finger1.createPointerDown(PointerInput.MouseButton.LEFT.asArg())) // move finger down and click (similar to mouse left click)
+                .addAction(new Pause(finger1, Duration.ofMillis(100))) // click and hold for a small duration
+                .addAction(finger1.createPointerUp(PointerInput.MouseButton.LEFT.asArg())); // move finger away from button
+
+        driver.perform(Collections.singletonList(sequence));
     }
 
     private Point getCenterOfElement(Point location, Dimension size) {
