02c0323c366a59efaa6cc909623e315bf4343742
Nigel <nigeldave19@gmail.com>
2024-04-16 22:14:22 +0530

    Create DriverFactory.java

---
+++ src/main/java/org/nigel/automation/factories/DriverFactory.java
@@ -0,0 +1,21 @@
+package org.nigel.automation.factories;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class DriverFactory {
+
+    private static final Logger log = LogManager.getLogger(DriverFactory.class.getName());
+
+    /**
+     * Private constructor to avoid external instantiation
+     */
+    private DriverFactory() {
+    }
+
+
+
+
+
+
+}
